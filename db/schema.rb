c7d40cf0ecdd249577bf466a94d1af3c458af701
Bharat <bharat.hegde@bugcrowd.com>
2024-08-25 16:30:02 +0530

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_23_091239) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.text "name", null: false
     t.integer "category_id", null: false
